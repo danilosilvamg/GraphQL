7875b7a4ae1bc3ac1349f65227e5121de7a8df55
Danilo Silva <danilo.net2009@gmail.com>
2022-05-24 18:49:48 -0300

    'atualizacao'

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,24 +1,46 @@
 import express from 'express';
-import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
+import  typeDefs  from './graphql/typeDefs'
+import  resolvers  from './graphql/resolvers'
 
-const server = express();
+const app = express();
 
-server.get('/status', (_, response) => {
-    response.send({
-        status:'okay',
-    });
+const server = new ApolloServer ({
+    typeDefs,
+    resolvers,
+
+    resolvers: {
+        Query: {
+            demands: () => [],
+        },
+    },
+
 });
 
-const enableCors = cors({ origin: 'http://localhost:3000' });
-
-server
-.options('/authenticate', enableCors)
-.post('/authenticate', enableCors, express.json(),(request, response) => {
-    console.log('E-mail', request.body.email, 'Senha', request.body.password );
-    response.send({
-        Okay: true,
-    });
+server.applyMiddleware({
+    app,
+    cors: {
+        origin: 'http://localhost:3000',
+    },
+    bodyParseConfig: true,
 });
+
+// server.get('/status', (_, response) => {
+//     response.send({
+//         status:'okay',
+//     });
+// });
+
+
+
+// server
+// .options('/authenticate', enableCors)
+// .post('/authenticate', enableCors, express.json(),(request, response) => {
+//     console.log('E-mail', request.body.email, 'Senha', request.body.password );
+//     response.send({
+//         Okay: true,
+//     });
+// });
 
 
 const PORT = process.env.PORT? parseInt(process.env.PORT) : 8000;
