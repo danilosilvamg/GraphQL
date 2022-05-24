918c815c418759e5f5d4b00601422015c5a88b77
Danilo Silva <danilo.net2009@gmail.com>
2022-05-24 10:41:54 -0300

    subindo para o github

---
+++ packages/web/src/pages/SignIn.js
@@ -0,0 +1,63 @@
+import {useState} from 'react';
+
+export default function SignIn(){
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://127.0.0.1:8000/authenticate', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'applicantion.json',
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        })
+
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Sucess', data);
+        });
+    };
+
+    const handleEmailChange = (event) => setEmail(event.target.value);
+
+    const handlePasswordChange = (event) => setPassword(event.target.value);
+
+    return(
+        <form onSubmit = {handleSubmit}>
+
+        <fieldset>
+            <label htmlfor="email">Email</label>
+            <input 
+            id="email" 
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            inputmode='email'
+            autoComplete="username" 
+            />
+        </fieldset>
+
+        <fieldset>
+            <label htmlfor="password">Senha</label>
+            <input 
+            id="password" 
+            type="password" 
+            autoComplete="current-password"
+            value={password} 
+            onChange={handlePasswordChange}
+            />
+        </fieldset>
+
+        <button type="submit">Entrar</button>
+
+    </form>
+
+    );
+        
+    
+}
